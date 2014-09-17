81488b304116c7c239558fd67b251394e6a95dbf
Darko Mikulic <darko.mikulic@gmail.com>
2014-09-18 00:51:48 +0200

    Shorten URL added to router

--- getshorty/urls.py
+++ getshorty/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'getshorty.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     #url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^shorten$', 'shorty.views.shorten', name='shorten'),
 )
