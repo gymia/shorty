5706202bf8da72881e1037c160aa4d39145e25e2
Darko Mikulic <darko.mikulic@gmail.com>
2014-09-18 01:21:45 +0200

    Initial models: ShortURL

--- shorty/models.py
+++ shorty/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class ShortURL(models.Model):
+    url           = models.CharField(max_length=2048)
+    shortcode     = models.CharField(max_length=128, db_index=True, unique=True)
+    startDate     = models.DateTimeField(auto_now_add=True)
+    lastSeenDate  = models.DateTimeField(auto_now_add=True, auto_now=True)
+    redirectCount = models.PositiveIntegerField(default=0)
+
+    def get_stats(self):
+        stats = {
+            "startDate": self.startDate.isoformat(),
+            "redirectCount": self.redirectCount
+        }
+        if self.redirectCount > 0:
+            stats["lastSeenDate"] = self.lastSeenDate.isoformat()
+        return stats
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.redirectCount += 1           
+        super(ShortURL, self).save(*args, **kwargs)
+
+    def __repr__(self):
+        return "<%s : %s>" % (self.shortcode, self.url)
+        
