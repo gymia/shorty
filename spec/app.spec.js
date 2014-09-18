bbf458731831998eb03f333f689642aa03a126b3
Spect <spect88@gmail.com>
2014-09-18 20:38:15 +0200

    Add URLShortener service and GET /:shortcode route
    I've made app module return a function which has to be called with
    all of the app's dependecies, so that a fake service can be injected
    easily.

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,3 +1,33 @@
-var app = require('../lib/app.js');
 var request = require('supertest');
+var http = require('http');
 
+// fake dependencies
+var shortener = {};
+
+// prepare app
+var app = require('../lib/app.js')(shortener);
+
+describe('App', function() {
+
+  describe('GET /:shortcode', function() {
+
+    it('returns 404 when shortcode cannot be found', function(done) {
+      shortener.getURLByShortcode = function() { return null; };
+      request(app)
+        .get('/nonExisting')
+        .expect(404)
+        .end(done);
+    });
+
+    it('redirects with 302 when shortcode is resolved', function(done) {
+      var url = 'http://www.example.com';
+      shortener.getURLByShortcode = function() { return url; };
+      request(app)
+        .get('/existing')
+        .expect(302)
+        .expect('Location', url)
+        .end(done);
+    });
+
+  });
+});
