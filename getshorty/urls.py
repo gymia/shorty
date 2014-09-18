e59ac5009101e072c81ef96c12a9dc5104075a12
Darko Mikulic <darko.mikulic@gmail.com>
2014-09-18 02:47:51 +0200

    Adding redirect, and stats to the router

--- getshorty/urls.py
+++ getshorty/urls.py
@@ -7,5 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     #url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^shorten$', 'shorty.views.shorten', name='shorten'),
+    url(r'^shorten$',                               'shorty.views.shorten',       name='shorten'),
+    url(r'^(?P<shortcode>[0-9a-zA-Z_]{1,})$',       'shorty.views.redirect',      name='redirect'),
+    url(r'^(?P<shortcode>[0-9a-zA-Z_]{1,})/stats$', 'shorty.views.shorten_stats', name='shorten_stats'),
 )
