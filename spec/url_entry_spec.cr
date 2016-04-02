922a4aa81fbf2402678ab94c990446dc854f8685
Umur Gedik <umurgdk@gmail.com>
2016-04-03 01:16:38 +0200

    UrlEntry now extracts its stats

--- spec/url_entry_spec.cr
+++ spec/url_entry_spec.cr
@@ -2,14 +2,20 @@ require "./spec_helper"
 
 describe Shorty::UrlEntry do
   it "should be able to increment last seen and redirect count data on visit" do
-    code = Shorty::CodeGenerator.new().generate
-
-    url_entry = Shorty::UrlEntry.new(code, "http://google.com")
+    url_entry = Shorty::UrlEntry.new("goog", "http://google.com")
 
     Fiber.sleep(0.00001)
     visited_entry = url_entry.visit
 
     (visited_entry.last_seen_at > url_entry.last_seen_at).should be_true
     (visited_entry.redirect_count > url_entry.redirect_count).should be_true
+  end
+
+  it "should extract lastSeenDate only for visited url entries" do
+    url_entry = Shorty::UrlEntry.new("googl", "http://google.com")
+    url_entry.stats["lastSeenDate"]?.should be_nil
+
+    visited_url_entry = url_entry.visit
+    visited_url_entry.stats["lastSeenDate"].should_not be_nil
   end
 end
--- src/shorty/url_entry.cr
+++ src/shorty/url_entry.cr
@@ -1,3 +1,5 @@
+ISO8016 = "%FT%TZ"
+
 class Shorty::UrlEntry
   getter code
   getter url
@@ -5,10 +7,23 @@ class Shorty::UrlEntry
   getter last_seen_at
   getter redirect_count
 
-  def initialize(@code, @url, @created_at = Time.now, @last_seen_at = Time.now, @redirect_count = 0)
+  def initialize(@code, @url, @created_at = Time.now.to_utc, @last_seen_at = Time.now.to_utc, @redirect_count = 0)
   end
 
   def visit
-    UrlEntry.new(@code, @url, @created_at, Time.now, @redirect_count + 1)
+    UrlEntry.new(@code, @url, @created_at, Time.now.to_utc, @redirect_count + 1)
+  end
+
+  def stats
+    data = { 
+      "startDate" => @created_at.to_s(ISO8016),
+      "redirectCount" => @redirect_count
+    }
+
+    if @redirect_count > 0
+      data["lastSeenDate"] = @last_seen_at.to_s(ISO8016)
+    end
+
+    data
   end
 end
