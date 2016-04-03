f7cc82caa2421149705924bf94e66fb9467f1e9e
Moo.gl <moo-gl@users.noreply.github.com>
2016-04-04 02:11:28 +0300

    Add koa-bodyparser middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 "use strict";
 
 const koa = require("koa");
+const bodyParser = require("koa-bodyparser");
 
 module.exports = function () {
   const app = koa();
 
+  app.use(bodyParser());
   return app;
 }
