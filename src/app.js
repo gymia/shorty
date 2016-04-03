b8b3fceea2daf1086a51ffe8b545b79cfc5e5928
Moo.gl <moo-gl@users.noreply.github.com>
2016-04-04 02:12:30 +0300

    Add route stubs

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 
 const koa = require("koa");
 const bodyParser = require("koa-bodyparser");
+const koaRouter = require("koa-router");
+
+function createRouter() {
+  const router = koaRouter();
+
+  router.post("/shorten", function *() {
+    this.body = {};
+  });
+
+  router.get("/:shortcode", function *() {
+    this.body = {};
+  });
+
+  router.get("/:shortcode/stats", function *() {
+    this.body = {};
+  });
+
+  return router;
+}
 
 module.exports = function () {
   const app = koa();
 
   app.use(bodyParser());
+
+  const router = createRouter();
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+
   return app;
-}
+};
