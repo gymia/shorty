6acc1695287287254cb76984c7f8aa542340b264
Umur Gedik <umurgdk@gmail.com>
2016-04-06 01:11:41 +0200

    Add json serialization for UrlEntry

--- spec/url_entry_spec.cr
+++ spec/url_entry_spec.cr
@@ -18,4 +18,41 @@ describe Shorty::UrlEntry do
     visited_url_entry = url_entry.visit
     visited_url_entry.stats["lastSeenDate"].should_not be_nil
   end
+
+  it "should be able to serialize itself as json" do
+    now = Time.now.to_utc
+    epoch = now.epoch
+
+    url_entry = Shorty::UrlEntry.new("googl", "http://google.com", now, now)
+
+    json = url_entry.to_json
+
+    hash = {
+      "url"            => "http://google.com",
+      "created_at"     => epoch,
+      "last_seen_at"   => epoch,
+      "redirect_count" => 0
+    }
+
+    json.should eq(hash.to_json)
+  end
+
+  it "should be able to deserialize itself from json" do
+    now = Time.now.to_utc
+    epoch = now.epoch
+
+    json_str = {
+      "url"            => "http://google.com",
+      "created_at"     => epoch,
+      "last_seen_at"   => epoch,
+      "redirect_count" => 0
+    }.to_json
+
+    entry = Shorty::UrlEntry.from_json("goog", json_str)
+
+    entry.url.should eq("http://google.com")
+    entry.created_at.epoch.should eq(epoch)
+    entry.last_seen_at.epoch.should eq(epoch)
+    entry.redirect_count.should eq(0)
+  end
 end
--- src/shorty/url_entry.cr
+++ src/shorty/url_entry.cr
@@ -1,3 +1,6 @@
+require "json"
+require "time"
+
 ISO8016 = "%FT%TZ"
 
 class Shorty::UrlEntry
@@ -24,6 +27,28 @@ class Shorty::UrlEntry
       data["lastSeenDate"] = @last_seen_at.to_s(ISO8016)
     end
 
-    data
+    return data
+  end
+
+  def self.from_json(code, json_str)
+    json = JSON.parse(json_str)
+
+    url = json["url"].to_s
+    created_at = Time.epoch(json["created_at"].as_i)
+    last_seen_at = Time.epoch(json["last_seen_at"].as_i)
+    redirect_count = json["redirect_count"].as_i
+
+    UrlEntry.new(code, url, created_at, last_seen_at, redirect_count)
+  end
+
+  def to_json
+    String.build do |io|
+      io.json_object do |obj|
+        obj.field "url", @url
+        obj.field "created_at", @created_at.epoch
+        obj.field "last_seen_at", @last_seen_at.epoch
+        obj.field "redirect_count", @redirect_count
+      end
+    end
   end
 end
