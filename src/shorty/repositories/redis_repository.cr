3f5e56f9e525322ff7813ebe88dc0c924f2d359b
Umur Gedik <umurgdk@gmail.com>
2016-04-06 01:39:38 +0200

    Add basic redis integration - RedisRepository

---
+++ src/shorty/repositories/redis_repository.cr
@@ -0,0 +1,23 @@
+require "redis"
+require "json"
+
+require "./base_repository"
+
+class Shorty::RedisRepository < Shorty::BaseRepository
+  def initialize(@connection : Redis)
+  end
+
+  def get(code)
+    @connection.get(code).try do |json|
+      UrlEntry.from_json(code, json)
+    end
+  end
+
+  def put(url_entry)
+    @connection.set(url_entry.code, url_entry.to_json)
+  end
+
+  def exists?(code)
+    !@connection.get(code).nil?
+  end
+end
