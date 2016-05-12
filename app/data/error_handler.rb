bb33e3af42574ac5e0dd4ea37ab040e7441659b1
Frank <fr.levering@gmail.com>
2016-05-12 22:08:11 +0200

    removed whitespace

--- app/data/error_handler.rb
+++ app/data/error_handler.rb
@@ -4,7 +4,6 @@ module Sinatra
   module ErrorHandler
 
     def self.registered(app)
-
       app.error 500 do
         status 500
         json :message => "Something went wrong. Please try again later"
