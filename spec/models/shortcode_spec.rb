b8de39f942bb66cf9cbabcc3cfc1a9892f9daa41
Filip Andonovski <filip@coqtail.com>
2016-07-02 12:25:49 +0200

    Shortcode model spec implemented

--- spec/models/shortcode_spec.rb
+++ spec/models/shortcode_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Shortcode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let(:shortcode) { FactoryGirl.create(:shortcode) }
+    let(:shortcode_without_shortcode) { FactoryGirl.create(:shortcode, shortcode: nil) }
+    let(:shortcode_with_invalid_shortcode) { FactoryGirl.build(:shortcode, shortcode: 'e#a^p!e') }
+    it { is_expected.to validate_presence_of(:url) }
+    it { is_expected.to validate_uniqueness_of(:shortcode) }
+    it { expect(shortcode.shortcode).to match(/^[0-9a-zA-Z_]{4,}$/) }
+    it { expect(shortcode_without_shortcode.shortcode).to match(/^[0-9a-zA-Z_]{6}$/) }
+    it "raises an error if shortcode does not meet the regex" do
+      shortcode_with_invalid_shortcode.valid?
+      shortcode_with_invalid_shortcode.errors.full_messages.should include("The shortcode fails to meet the following regexp: ^[0-9a-zA-Z_]{4,}$.")
+    end
+  end
 end
