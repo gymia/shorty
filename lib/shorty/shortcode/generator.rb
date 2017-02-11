092d75a4a2da7d5aeefbeaf7174ff4fd68e82663
Mauricio Voto <mauriciovoto@gmail.com>
2017-02-11 13:55:48 -0200

    fix generator logic according to requirements
    - use of Base64#urlsafe_encode to remove the randomness
    - add http if url does not have it(so we always have a minimum sized url
    string)
    - add reverse to match url, so we won't have repeated shortcodes for
    short urls(due to http prefix)

--- lib/shorty/shortcode/generator.rb
+++ lib/shorty/shortcode/generator.rb
@@ -1,11 +1,12 @@
+require 'base64'
+
 module Shorty
   module Shortcode
     class Generator
-      # TODO: check if generated code already exists(if so, re-generate it)
-      def self.run
-        # ASCII 2^8 == 256 == 0..255(thanks internets :p)
-        # All ASCII chars that matches the regex are taken randomly from sample
-        (0..255).map(&:chr).select{|x| x =~ /[0-9a-zA-Z_]/}.sample(6).join
+      def self.run(url)
+        url = "http://#{url}" unless url =~ /http/
+        encoded = Base64.urlsafe_encode64(url)
+        encoded.reverse.scan(/[0-9a-zA-Z_]{6}/).last
       end
     end
   end
--- spec/shorty/shortcode/generator_spec.rb
+++ spec/shorty/shortcode/generator_spec.rb
@@ -2,8 +2,29 @@ require 'spec_helper'
 
 describe Shorty::Shortcode::Generator do
   describe '.run' do
-    it 'generates a valid random shortcode' do
-      expect(described_class.run).to match(/^[0-9a-zA-Z_]{6}$/)
+    context 'given a very short url' do
+      subject { described_class.run("1") }
+
+      it { is_expected.to match(/^[0-9a-zA-Z_]{6}$/) }
+      it { is_expected.to eq("EzLvoD") }
+    end
+
+    context 'given another very short url' do
+      subject { described_class.run("a") }
+
+      it { is_expected.to match(/^[0-9a-zA-Z_]{6}$/) }
+      it { is_expected.to eq("E2LvoD") }
+    end
+
+    context 'given a bigger url' do
+      subject do
+        described_class.run(
+          "http://steamcommunity.com/sharedfiles/filedetails/?id=857773350"
+        )
+      end
+
+      it { is_expected.to match(/^[0-9a-zA-Z_]{6}$/) }
+      it { is_expected.to eq("Dc0RHa") }
     end
   end
 end
