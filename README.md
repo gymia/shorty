b5831e9d0fed8eec696d4760f571104e4c15899a
Yoel Blum <yoelblumenator@gmail.com>
2017-10-04 11:42:54 -0700

    created readme and simple build script

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-build.sh
-rackup
-
-#tests
-rspec
+#build the project 
+1)checkout my branch
+2)run: build.sh
+#start the server
+ rackup
+#run the tests
+ rspec
--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+echo "===== Installing packages ======="
 sudo apt-get update
 sudo apt-get install git-core curl build-essential libssl-dev libreadline-dev -y
-sudo apt-add-repository ppa:brightbox/ruby-ng -y
 sudo apt-get install libyaml-dev libsqlite3-dev sqlite3  python-software-properties software-properties-common -y
-sudo apt-get install patch ruby-dev zlib1g-dev liblzma-dev -y
+sudo apt-get install patch zlib1g-dev liblzma-dev -y
+sudo apt-add-repository ppa:brightbox/ruby-ng -y
+echo "#===== Installing Ruby ========"
+sleep 2
+sudo apt-get update
 sudo apt-get install ruby2.4 ruby2.4-dev -y
 sudo gem install bundler
 sudo gem install nokogiri
+echo "#====== Installing Gems ======"
 bundle install
 rake db:create
 rake db:migrate
